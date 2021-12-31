47070f3d4d5fba58c672619094c12a0750b81fc5
Akiyo <66089327+Akiyo-bot@users.noreply.github.com>
2021-12-31 13:57:52 +0100

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,14 +1,1 @@
-const { MessageEmbed } = require("discord.js");
-const db = require('quick.db');
-const discord = require("discord.js");
-const moment = require("moment");
-
-module.exports = async (client, guild) => {
-    db.delete(`${guild.id}.config.prefix`, '$')
-
-    const newGuildEmbed = new MessageEmbed()
-        .setColor('GREEN')
-        .setAuthor(guild.name, guild.iconURL({ dynamic: true }))
-        .setDescription(`J'ai quitter le serveur **${guild.name}** !\nJe suis maintenant dans **` + client.guilds.cache.size + `** serveurs !`)
-        client.users.cache.get("770651879838449665").send(newGuildEmbed);
-}
+PRIVATE
